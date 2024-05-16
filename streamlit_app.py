8b9c5a3d7c4838c0d8a4ae665efc929c63434b38
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-05-16 16:45:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("Hola mundo")
+nombre = st.text_input("Introduzca su nombre",value="nombre")
+fecha_nac = st.date_input("Fecha de nacimiento")
+
+texto = f'''Nombre: {nombre}
+Fecha de nacimiento: {fecha_nac}
+'''
+
+# Different ways to use the API
+
+if st.download_button('Descargar', texto):
+  st.write("Descarga realizada")
